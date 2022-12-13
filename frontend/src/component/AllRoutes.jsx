246aee85d285acb064f1cf510e4e30007df54095
Sarsande-Sunil <priyankaingle250@gmail.com>
2022-12-13 23:03:37 +0530

    added

---
+++ frontend/src/component/AllRoutes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path=""></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default AllRoutes;
