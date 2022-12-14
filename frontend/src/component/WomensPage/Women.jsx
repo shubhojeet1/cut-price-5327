a296dc354a975e3330333c3476596f45e114e920
Shubh596 <pandeyaashu4@gmail.com>
2022-12-15 00:35:43 +0530

    Day 2

---
+++ frontend/src/component/WomensPage/Women.jsx
@@ -0,0 +1,11 @@
+import "./women.css"
+
+export default function Women() {
+  return (
+    <div>
+<div><img src="https://www.yoox.com/images/yoox80/banners/6895_1_Genz_WM_Main.png?634485886601286852#width=1380&height=637" alt="" /></div>
+
+
+    </div>
+  )
+}
