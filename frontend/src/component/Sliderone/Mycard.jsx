a1dd80c7951678fb838be6f75f6308e341af5d43
iam-souvik <souvikpatramax@gmail.com>
2022-12-14 23:19:23 +0530

    day_1_task_souvik

---
+++ frontend/src/component/Sliderone/Mycard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Mycard.css"
+
+const Mycard = (props) => {
+
+ 
+
+
+  return (
+    <div className='maindiv'>
+     
+      Mycard No. {props.cardno}
+    </div>
+  )
+}
+
+export default Mycard
